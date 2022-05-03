f1131386a896d9fdc3f7b2af37576efd593a7c57
Raven <rydela0106@gmail.com>
2022-05-02 20:14:56 -0500

    second commit.

---
+++ src/com/company/Checkbox.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Checkbox {
+
+    private String question;
+    private String correctAnswer;
+    private ArrayList<String> answerOptions;
+
+    // answer options
+    // point value
+
+    // what is your favorite color?
+    // a: blue, b: green, c: red, d: purple
+    // blue, green, red, purple
+
+    // grade question
+    // ask question
+}
